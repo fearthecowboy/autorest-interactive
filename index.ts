87f0f32751cc334dc24fbf58dc9a802b62b70400
Garrett Serack <gserack@gmail.com>
2017-11-09 14:23:17 -0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import * as electron from "electron";
 import { spawn } from "child_process";
 
 if (typeof electron === "string") {
+  // make sure that electron can be electron!
+  delete process.env['ELECTRON_RUN_AS_NODE'];
+  
   spawn(electron as any, [__filename], { stdio: ["ignore", "ignore", process.stderr, process.stdin, process.stdout] });
 } else {
   electron.app.on("ready", () => {
